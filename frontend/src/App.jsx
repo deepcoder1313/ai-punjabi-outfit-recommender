20b5dec15fea9927960a0c75f34ebace2b4ecf21
Sandeep Singh <jaspalshubh12@gmail.com>
2026-05-15 13:58:57 +0530

    skg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -210,29 +210,8 @@ function App() {
                   </div>
                 ))}
 
-              {/* FITTI */}
-              {data.fitti_images && data.fitti_images.length > 0 && (
-  <div className="section-block">
-    
-    <h3 className="section-title">
-      <span className="section-dot fitti" />
-      Fitti – {data.fitti_color}
-    </h3>
-
-   
-
- 
-
-                  
-<div className="fitti-container">
-  {data.fitti_images.map((img, i) => (
-    <div className="fitti-card" key={i}>
-      <img src={img} className="fitti-img" />
-    </div>
-  ))}
-</div>
-               </div>  
-              )}
+             
+              
             </div>
           )}
         </section>
